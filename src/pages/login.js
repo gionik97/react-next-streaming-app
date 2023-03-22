f633467d0b640ac887b08d2f36fc6f88306bf366
gionik97 <45343354+gionik97@users.noreply.github.com>
2023-03-22 17:21:28 +0400

    Create ( Pages - Login ) - setting up login page

---
+++ src/pages/login.js
@@ -0,0 +1,8 @@
+import { useStateContext } from "components/HBOProvider";
+import Login from "components/ui/Login/Login";
+
+const LoginPage = () => {
+  return <Login />;
+};
+
+export default LoginPage;
