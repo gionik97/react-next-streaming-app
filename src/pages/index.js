5a35adfee456a7fd88fc4d9c14f93406b4657637
gionik97 <45343354+gionik97@users.noreply.github.com>
2023-03-22 17:20:49 +0400

    Refactor ( Page - Index ) - setting up index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,25 @@
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 import { useStateContext } from "components/HBOProvider";
-import Login from "components/ui/Login/Login";
+import MainLayout from "components/layouts/MainLayout";
+import FeaturedMedia from "components/ui/FeaturedMedia/FeaturedMedia";
+import ForYouList from "components/ui/ForYouList/ForYouList";
+import JustAdded from "components/ui/JustAdded/JustAdded";
+import PosterView from "components/ui/PosterView/PosterView";
+import AuthCheck from "components/AuthCheck";
 
 export default function Home() {
   const globalState = useStateContext();
   const router = useRouter();
 
-  useEffect(() => {
-    const loggedIn = false;
-    if (loggedIn === false) {
-      router.push("/create");
-    }
-  }, []);
+  useEffect(() => {}, []);
 
-  return (
-    <div>
-      <Login />
-    </div>
+  return AuthCheck(
+    <MainLayout>
+      <FeaturedMedia />
+      <ForYouList />
+      <JustAdded />
+      <PosterView />
+    </MainLayout>
   );
 }
