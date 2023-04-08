acb50ff30e8daf7bdf4a157a36ee36e23bb41ee7
gionik97 <45343354+gionik97@users.noreply.github.com>
2023-04-08 15:58:15 +0400

    Feature ( pages - index ) - adding lazyload to MediaRow compoment

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import { useEffect } from "react";
 import { useRouter } from "next/router";
+import LazyLoad from "parm-react-lazyload";
 import { useStateContext } from "components/HBOProvider";
 import MainLayout from "components/layouts/MainLayout";
 import FeaturedMedia from "components/ui/FeaturedMedia/FeaturedMedia";
 import AuthCheck from "components/AuthCheck";
 import MediaRow from "components/ui/MediaRow/MediaRow";
+import Placeholder from "components/ui/Placeholder/Placeholder";
 
 export default function Home() {
   const globalState = useStateContext();
@@ -15,31 +17,56 @@ export default function Home() {
   return AuthCheck(
     <MainLayout>
       <FeaturedMedia />
-      <MediaRow
-        title="Movies"
-        type="large-v"
-        endpoint="discover/movie?with_genres=28&primary_release_year=2022"
-      />
-      <MediaRow
-        title="Series"
-        type="small-h"
-        endpoint="discover/movie?with_genres=28&primary_release_year=2022"
-      />
-      <MediaRow
-        title="Action"
-        type="small-v"
-        endpoint="discover/movie?with_genres=28&primary_release_year=2022"
-      />
-      <MediaRow
-        title="Horror"
-        type="large-v"
-        endpoint="discover/movie?with_genres=27&primary_release_year=2022"
-      />
-      <MediaRow
-        title="Sci-fi"
-        type="small-v"
-        endpoint="discover/movie?with_genres=878&primary_release_year=2022"
-      />
+      <LazyLoad
+        offset={-400}
+        placeholder={<Placeholder title="Movies" type="large-v" />}
+      >
+        <MediaRow
+          title="Movies"
+          type="large-v"
+          endpoint="discover/movie?sort_by=popularity.desc&primary_release_year=2022"
+        />
+      </LazyLoad>
+      <LazyLoad
+        offset={-400}
+        placeholder={<Placeholder title="Series" type="small-h" />}
+      >
+        <MediaRow
+          title="Series"
+          type="small-h"
+          endpoint="discover/tv?primary_release_year=2022"
+        />
+      </LazyLoad>
+      <LazyLoad
+        offset={-400}
+        placeholder={<Placeholder title="Action" type="small-v" />}
+      >
+        <MediaRow
+          title="Action"
+          type="small-v"
+          endpoint="discover/movie?with_genres=28&primary_release_year=2022"
+        />
+      </LazyLoad>
+      <LazyLoad
+        offset={-400}
+        placeholder={<Placeholder title="Horror" type="large-v" />}
+      >
+        <MediaRow
+          title="Horror"
+          type="large-v"
+          endpoint="discover/movie?with_genres=27&primary_release_year=2022"
+        />
+      </LazyLoad>
+      <LazyLoad
+        offset={-400}
+        placeholder={<Placeholder title="Sci-fi" type="small-v" />}
+      >
+        <MediaRow
+          title="Sci-fi"
+          type="small-v"
+          endpoint="discover/movie?with_genres=878&primary_release_year=2022"
+        />
+      </LazyLoad>
     </MainLayout>
   );
 }
